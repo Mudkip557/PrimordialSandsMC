eed9f2bd285f96701edf9c9ee76dc004b1ed1458
Lime_Limeington <alexgamer@singularitypc.com>
2020-05-21 17:03:15 -0400

    MICKY"S MAPPY MEAL MUCH MEAT MODERATION MODIFIED

---
+++ src/main/java/net/mcreator/primordialsands/entity/EntityCreeperman.java
@@ -0,0 +1,216 @@
+
+package net.mcreator.primordialsands.entity;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.World;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.DamageSource;
+import net.minecraft.pathfinding.PathNavigateFlying;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.entity.projectile.EntityPotion;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.entity.ai.EntityFlyHelper;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAILeapAtTarget;
+import net.minecraft.entity.ai.EntityAIAttackMelee;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.model.ModelSlime;
+import net.minecraft.block.state.IBlockState;
+
+import net.mcreator.primordialsands.item.ItemMickysmappymeal;
+import net.mcreator.primordialsands.ElementsPrimordialSands;
+
+import java.util.Random;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@ElementsPrimordialSands.ModElement.Tag
+public class EntityCreeperman extends ElementsPrimordialSands.ModElement {
+	public static final int ENTITYID = 2;
+	public static final int ENTITYID_RANGED = 3;
+	public EntityCreeperman(ElementsPrimordialSands instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void initElements() {
+		elements.entities.add(() -> EntityEntryBuilder.create().entity(EntityCustom.class)
+				.id(new ResourceLocation("primordialsands", "creeperman"), ENTITYID).name("creeperman").tracker(256, 3, true).egg(-1, -1).build());
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+		Biome[] spawnBiomes = allbiomes(Biome.REGISTRY);
+		EntityRegistry.addSpawn(EntityCustom.class, 20, 3, 30, EnumCreatureType.MONSTER, spawnBiomes);
+	}
+
+	private Biome[] allbiomes(net.minecraft.util.registry.RegistryNamespaced<ResourceLocation, Biome> in) {
+		Iterator<Biome> itr = in.iterator();
+		ArrayList<Biome> ls = new ArrayList<Biome>();
+		while (itr.hasNext())
+			ls.add(itr.next());
+		return ls.toArray(new Biome[ls.size()]);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		RenderingRegistry.registerEntityRenderingHandler(EntityCustom.class, renderManager -> {
+			return new RenderLiving(renderManager, new ModelSlime(0), 0.5f) {
+				protected ResourceLocation getEntityTexture(Entity entity) {
+					return new ResourceLocation("primordialsands:textures/empty_middle.png");
+				}
+			};
+		});
+	}
+	public static class EntityCustom extends EntityCreeper {
+		public EntityCustom(World world) {
+			super(world);
+			setSize(1f, 1f);
+			experienceValue = 10;
+			this.isImmuneToFire = true;
+			setNoAI(!true);
+			this.navigator = new PathNavigateFlying(this, this.world);
+			this.moveHelper = new EntityFlyHelper(this);
+		}
+
+		@Override
+		protected void initEntityAI() {
+			super.initEntityAI();
+			this.tasks.addTask(1, new EntityAIWander(this, 1));
+			this.tasks.addTask(2, new EntityAILookIdle(this));
+			this.tasks.addTask(3, new EntityAISwimming(this));
+			this.tasks.addTask(4, new EntityAIWander(this, 1, 20) {
+				@Override
+				protected Vec3d getPosition() {
+					Random random = EntityCustom.this.getRNG();
+					double dir_x = EntityCustom.this.posX + ((random.nextFloat() * 2 - 1) * 16);
+					double dir_y = EntityCustom.this.posY + ((random.nextFloat() * 2 - 1) * 16);
+					double dir_z = EntityCustom.this.posZ + ((random.nextFloat() * 2 - 1) * 16);
+					return new Vec3d(dir_x, dir_y, dir_z);
+				}
+			});
+			this.tasks.addTask(5, new EntityAILeapAtTarget(this, (float) 1));
+			this.tasks.addTask(6, new EntityAIAttackMelee(this, 1.5, true));
+		}
+
+		@Override
+		public EnumCreatureAttribute getCreatureAttribute() {
+			return EnumCreatureAttribute.UNDEAD;
+		}
+
+		@Override
+		protected Item getDropItem() {
+			return new ItemStack(ItemMickysmappymeal.block, (int) (1)).getItem();
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getAmbientSound() {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation(""));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getHurtSound(DamageSource ds) {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("block.cloth.hit"));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getDeathSound() {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("block.grass.break"));
+		}
+
+		@Override
+		protected float getSoundVolume() {
+			return 1.0F;
+		}
+
+		@Override
+		public void fall(float l, float d) {
+		}
+
+		@Override
+		public boolean attackEntityFrom(DamageSource source, float amount) {
+			if (source.getImmediateSource() instanceof EntityArrow)
+				return false;
+			if (source.getImmediateSource() instanceof EntityPotion)
+				return false;
+			if (source == DamageSource.FALL)
+				return false;
+			if (source == DamageSource.CACTUS)
+				return false;
+			if (source == DamageSource.DROWN)
+				return false;
+			if (source == DamageSource.LIGHTNING_BOLT)
+				return false;
+			return super.attackEntityFrom(source, amount);
+		}
+
+		@Override
+		protected void applyEntityAttributes() {
+			super.applyEntityAttributes();
+			if (this.getEntityAttribute(SharedMonsterAttributes.ARMOR) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(1D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.9D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(2D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(10D);
+			this.getAttributeMap().registerAttribute(SharedMonsterAttributes.FLYING_SPEED);
+			this.getEntityAttribute(SharedMonsterAttributes.FLYING_SPEED).setBaseValue(0.9);
+		}
+
+		@Override
+		public boolean canBreatheUnderwater() {
+			return true;
+		}
+
+		@Override
+		public boolean getCanSpawnHere() {
+			return true;
+		}
+
+		@Override
+		public boolean isNotColliding() {
+			return this.world.checkNoEntityCollision(this.getEntityBoundingBox(), this);
+		}
+
+		@Override
+		public boolean isPushedByWater() {
+			return false;
+		}
+
+		@Override
+		public void onUpdate() {
+			super.onUpdate();
+			this.setNoGravity(true);
+		}
+
+		@Override
+		protected void updateFallState(double y, boolean onGroundIn, IBlockState state, BlockPos pos) {
+		}
+
+		@Override
+		public void setNoGravity(boolean ignored) {
+			super.setNoGravity(true);
+		}
+	}
+}
