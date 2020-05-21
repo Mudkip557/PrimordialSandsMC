9dfcf73b8579c9ce843f1694c630f72703803eaf
supertek65 <bradmopp@gmail.com>
2020-05-21 17:42:27 -0500

    Added a cursed mob

--- src/main/java/net/mcreator/primordialsands/entity/EntityCreeperman.java
+++ src/main/java/net/mcreator/primordialsands/entity/EntityCreeperman.java
@@ -33,7 +33,9 @@ import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.Entity;
 import net.minecraft.client.renderer.entity.RenderLiving;
-import net.minecraft.client.model.ModelSlime;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.model.ModelBox;
+import net.minecraft.client.model.ModelBase;
 import net.minecraft.block.state.IBlockState;
 
 import net.mcreator.primordialsands.procedure.ProcedureCreepermanRightClickedOnEntity;
@@ -76,9 +78,9 @@ public class EntityCreeperman extends ElementsPrimordialSands.ModElement {
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
 		RenderingRegistry.registerEntityRenderingHandler(EntityCustom.class, renderManager -> {
-			return new RenderLiving(renderManager, new ModelSlime(0), 0.5f) {
+			return new RenderLiving(renderManager, new ModelVengefulSoul(), 0.5f) {
 				protected ResourceLocation getEntityTexture(Entity entity) {
-					return new ResourceLocation("primordialsands:textures/empty_middle.png");
+					return new ResourceLocation("primordialsands:textures/98262a33ce21ea98.png");
 				}
 			};
 		});
@@ -86,7 +88,7 @@ public class EntityCreeperman extends ElementsPrimordialSands.ModElement {
 	public static class EntityCustom extends EntityCreeper {
 		public EntityCustom(World world) {
 			super(world);
-			setSize(1f, 1f);
+			setSize(1f, 2f);
 			experienceValue = 10;
 			this.isImmuneToFire = true;
 			setNoAI(!true);
@@ -239,6 +241,50 @@ public class EntityCreeperman extends ElementsPrimordialSands.ModElement {
 		@Override
 		public void setNoGravity(boolean ignored) {
 			super.setNoGravity(true);
+		}
+	}
+
+	public static class ModelVengefulSoul extends ModelBase {
+		private final ModelRenderer Head;
+		private final ModelRenderer Body;
+		private final ModelRenderer RightArm;
+		private final ModelRenderer LeftArm;
+		public ModelVengefulSoul() {
+			textureWidth = 64;
+			textureHeight = 64;
+			Head = new ModelRenderer(this);
+			Head.setRotationPoint(0.0F, 0.0F, 0.0F);
+			Head.cubeList.add(new ModelBox(Head, 0, 0, -4.0F, -8.0F, -4.0F, 8, 8, 8, 0.0F, false));
+			Head.cubeList.add(new ModelBox(Head, 32, 0, -4.0F, -8.0F, -4.0F, 8, 8, 8, 0.5F, false));
+			Body = new ModelRenderer(this);
+			Body.setRotationPoint(0.0F, 0.0F, 0.0F);
+			setRotationAngle(Body, 0.5236F, 0.0F, 0.0F);
+			Body.cubeList.add(new ModelBox(Body, 16, 16, -4.0F, 0.0F, -2.0F, 8, 12, 4, 0.0F, false));
+			Body.cubeList.add(new ModelBox(Body, 16, 32, -4.0F, 0.0F, -2.0F, 8, 12, 4, 0.25F, false));
+			RightArm = new ModelRenderer(this);
+			RightArm.setRotationPoint(-5.0F, 2.0F, 0.0F);
+			setRotationAngle(RightArm, -0.4363F, 0.0F, 0.0F);
+			RightArm.cubeList.add(new ModelBox(RightArm, 40, 16, -3.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F, false));
+			RightArm.cubeList.add(new ModelBox(RightArm, 40, 32, -3.0F, -2.0F, -2.0F, 4, 12, 4, 0.25F, false));
+			LeftArm = new ModelRenderer(this);
+			LeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
+			setRotationAngle(LeftArm, -0.4363F, 0.0F, 0.0F);
+			LeftArm.cubeList.add(new ModelBox(LeftArm, 32, 48, -1.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F, false));
+			LeftArm.cubeList.add(new ModelBox(LeftArm, 48, 48, -1.0F, -2.0F, -2.0F, 4, 12, 4, 0.25F, false));
+		}
+
+		@Override
+		public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+			Head.render(f5);
+			Body.render(f5);
+			RightArm.render(f5);
+			LeftArm.render(f5);
+		}
+
+		public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+			modelRenderer.rotateAngleX = x;
+			modelRenderer.rotateAngleY = y;
+			modelRenderer.rotateAngleZ = z;
 		}
 	}
 }
